024658300c48650852fc4c7943bf75e2288cef1c
mario-ah-salamanca <mario.ah.salamanca@gmail.com>
2022-11-06 09:16:03 +0100

    testing channels

--- WaterMe_site/WaterMe_site/settings.py
+++ WaterMe_site/WaterMe_site/settings.py
@@ -124,4 +124,4 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-STATICFILES_DIRS = ( os.path.join(BASE_DIR, "static/"),)
+STATICFILES_DIRS = ( os.path.join(BASE_DIR, "statics/"),)
