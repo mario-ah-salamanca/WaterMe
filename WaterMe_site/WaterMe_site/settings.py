64f845337ddf713d8d45e69b3345484d9b79aec9
mario-ah-salamanca <mario.ah.salamanca@gmail.com>
2022-11-06 11:11:12 +0100

    puta ya funciona

--- WaterMe_site/WaterMe_site/settings.py
+++ WaterMe_site/WaterMe_site/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'WaterMe_site.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [os.path.join(BASE_DIR, 'templates'), os.path.join(BASE_DIR, 'templates/images')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
