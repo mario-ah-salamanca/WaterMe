cbada3bf9e9d25e474c6ffb6d6ef17510316c710
Kelan Garcia <kelanando@hotmail.com>
2022-11-06 03:26:47 +0100

    attempt to fix bug by reading sensor in request of homepage

--- WaterMe_site/WaterMe_site/views.py
+++ WaterMe_site/WaterMe_site/views.py
@@ -7,9 +7,26 @@ from django.contrib.auth.decorators import permission_required
 from django.http import JsonResponse
 from django.shortcuts import render, get_object_or_404, redirect
 from django.views import View
-from . import globals
 
-globals.initialize()
+import time
+import board
+import adafruit_dht
+
+#from . import globals
+#globals.initialize()
+
+# Initial the dht device, with data pin connected to:
+dhtDevice = adafruit_dht.DHT11(board.D22)
+
 
 def index(request):
-    return render(request, 'web_page.html', globals.data)
+    temperature_c = dhtDevice.temperature
+    temperature_f = temperature_c * (9 / 5) + 32
+    humidity = dhtDevice.humidity
+
+    data = {
+        'temperature_c': temperature_c,
+        'temperature_f': temperature_f,
+        'humidity': humidity
+    }
+    return render(request, 'web_page.html', data)
